45202446ae6c61dbe45478f943606dae6ffc212c
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2023-05-26 04:56:31 +0300

    stillcode second section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-// import components 
+// import components.
 import Navbar from '../components/navbar'; 
+
+// import photos.
+import spain  from "../assets/spain.png";
+import dubai  from "../assets/dubai.png";
+import giza  from "../assets/pyramid.png";
 
 const App = () => {
   return (
@@ -12,6 +17,27 @@ const App = () => {
         <div className="first">
           <h1>Welcome to <span>Travilia</span></h1>
           <p>The Best Travel Company You will See Ever !</p>
+        </div>
+
+        <br />
+        <hr />
+
+        <div className="second">
+          <h1>Our Programes</h1>
+          <div className="cards">
+            <div className="card">
+              <img src={spain} width='350px' height='200px'/>
+              <div className="title">Spain</div>
+            </div>
+            <div className="card">
+              <img src={dubai} width='350px' height='200px'/>
+              <div className="title">Dubai</div>
+            </div>
+            <div className="card">
+              <img src={giza} width='350px' height='200px'/>
+              <div className="title">Giza</div>
+            </div>
+          </div>
         </div>
       </div>
     </>
